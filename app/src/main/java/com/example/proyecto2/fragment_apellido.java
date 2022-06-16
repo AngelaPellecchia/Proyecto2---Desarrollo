2e7daf1e38deceab17aaf43f187feda5478ff5fd
D-Camarena <dcamarena.94@gmail.com>
2022-06-16 13:50:07 -0500

    Se agregó fragment_apellido y se ajustó la distribución de los fragments

---
+++ app/src/main/java/com/example/proyecto2/fragment_apellido.java
@@ -0,0 +1,35 @@
+package com.example.proyecto2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link fragment_apellido#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class fragment_apellido extends Fragment {
+
+
+    public fragment_apellido() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_apellido, container, false);
+    }
+}
